28654e61e2e356a021e9fb029bb71d8b561a1860
PaulSebalu <paolossebalu@gmail.com>
2022-01-22 08:54:00 +0300

    refactor validator

--- src/currencyExchange/validator.js
+++ src/currencyExchange/validator.js
@@ -2,7 +2,11 @@ import Joi from "joi";
 
 // eslint-disable-next-line import/prefer-default-export
 export const conversionSchema = Joi.object({
-  amount: Joi.number().positive().label("amount"),
-  baseCurrency: Joi.number().integer().positive().label("baseCurrency"),
+  amount: Joi.number().positive().required().label("amount"),
+  baseCurrency: Joi.number()
+    .integer()
+    .required()
+    .positive()
+    .label("baseCurrency"),
   exchangeCurrency: Joi.string().required().trim().label("exchangeCurrency"),
 });
